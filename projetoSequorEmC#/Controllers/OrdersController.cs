450780e9c1fd91aa8c283dca3b2f493825273776
rodrigopires83 <102845202+rodrigopires83@users.noreply.github.com>
2024-04-18 16:41:37 -0300

    Update OrdersController.cs

--- projetoSequorEmC#/Controllers/OrdersController.cs
+++ projetoSequorEmC#/Controllers/OrdersController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using projetoSequorEmC_.Services;
 using System.Text.Json.Nodes;
 
-namespace projetoSequorEmC_.Controllers
+namespace projetoSequorEmC_.Models
 {
     [ApiController]
     [Route("api/orders")]
